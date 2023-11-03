5c88220927b6ad10c035cd6fd49eac24d3c4f6e3
msdmhmdi <masoudmhmdi86@gmail.com>
2023-11-03 13:58:57 +0330

    feat:server.js
    add routing

--- server.js
+++ server.js
@@ -1,8 +1,70 @@
 import { createServer } from 'http';
-import { readFile } from 'fs/promises';
+import { readFile, readdir } from 'fs/promises';
 import escapeHtml from 'escape-html';
+import sanitizeFilename from 'sanitize-filename';
 
-function BlogPostPage({ postContent, author }) {
+createServer(async (req, res) => {
+  try {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const page = await matchRoute(url);
+    sendHTML(res, <BlogLayout>{page}</BlogLayout>);
+  } catch (err) {
+    console.error(err);
+    res.statusCode = err.statusCode ?? 500;
+    res.end();
+  }
+}).listen(8080);
+
+async function matchRoute(url) {
+  if (url.pathname === '/') {
+    const postFiles = await readdir('./posts');
+    const postSlugs = postFiles.map((file) => file.slice(0, file.lastIndexOf('.')));
+    const postContents = await Promise.all(
+      postSlugs.map((postSlug) => readFile('./posts/' + postSlug + '.txt', 'utf8'))
+    );
+    return <BlogIndexPage postSlugs={postSlugs} postContents={postContents} />;
+  } else {
+    const postSlug = sanitizeFilename(url.pathname.slice(1));
+    try {
+      const postContent = await readFile('./posts/' + postSlug + '.txt', 'utf8');
+      return <BlogPostPage postSlug={postSlug} postContent={postContent} />;
+    } catch (err) {
+      throwNotFound(err);
+    }
+  }
+}
+
+function BlogIndexPage({ postSlugs, postContents }) {
+  return (
+    <section>
+      <h1>Welcome to my blog</h1>
+      <div>
+        {postSlugs.map((postSlug, index) => (
+          <section key={postSlug}>
+            <h2>
+              <a href={'/' + postSlug}>{postSlug}</a>
+            </h2>
+            <article>{postContents[index]}</article>
+          </section>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+function BlogPostPage({ postSlug, postContent }) {
+  return (
+    <section>
+      <h2>
+        <a href={'/' + postSlug}>{postSlug}</a>
+      </h2>
+      <article>{postContent}</article>
+    </section>
+  );
+}
+
+function BlogLayout({ children }) {
+  const author = 'Jae Doe';
   return (
     <html>
       <head>
@@ -10,10 +72,10 @@ function BlogPostPage({ postContent, author }) {
       </head>
       <body>
         <nav>
-          <a href='/'>Home</a>
+          <a href="/">Home</a>
           <hr />
         </nav>
-        <article>{postContent}</article>
+        <main>{children}</main>
         <Footer author={author} />
       </body>
     </html>
@@ -32,47 +94,48 @@ function Footer({ author }) {
     </footer>
   );
 }
+
+function sendHTML(res, jsx) {
+  const html = renderJSXToHTML(jsx);
+  res.setHeader('Content-Type', 'text/html');
+  res.end(html);
+}
+
+function throwNotFound(cause) {
+  const notFound = new Error('Not found.', { cause });
+  notFound.statusCode = 404;
+  throw notFound;
+}
+
 function renderJSXToHTML(jsx) {
   if (typeof jsx === 'string' || typeof jsx === 'number') {
-    // This is a string. Escape it and put it into HTML directly.
     return escapeHtml(jsx);
   } else if (jsx == null || typeof jsx === 'boolean') {
-    // This is an empty node. Don't emit anything in HTML for it.
     return '';
   } else if (Array.isArray(jsx)) {
-    // This is an array of nodes. Render each into HTML and concatenate.
     return jsx.map((child) => renderJSXToHTML(child)).join('');
   } else if (typeof jsx === 'object') {
-    // Check if this object is a React JSX element (e.g. <div />).
     if (jsx.$$typeof === Symbol.for('react.element')) {
       if (typeof jsx.type === 'string') {
-        // Is this a tag like <div>?
-        // Existing code that handles HTML tags (like <p>).
         let html = '<' + jsx.type;
-        // ...
+        for (const propName in jsx.props) {
+          if (jsx.props.hasOwnProperty(propName) && propName !== 'children') {
+            html += ' ';
+            html += propName;
+            html += '=';
+            html += escapeHtml(jsx.props[propName]);
+          }
+        }
+        html += '>';
+        html += renderJSXToHTML(jsx.props.children);
         html += '</' + jsx.type + '>';
         return html;
       } else if (typeof jsx.type === 'function') {
-        // Is it a component like <BlogPostPage>?
-        // Call the component with its props, and turn its returned JSX into HTML.
         const Component = jsx.type;
         const props = jsx.props;
         const returnedJsx = Component(props);
         return renderJSXToHTML(returnedJsx);
       } else throw new Error('Not implemented.');
-    }
-  }
-}
-createServer(async (req, res) => {
-  const author = 'Jae Doe';
-  const postContent = await readFile('./posts/hello-world.txt', 'utf8');
-
-  sendHTML(res, <BlogPostPage postContent={postContent} author={author} />);
-}).listen(8080);
-
-function sendHTML(res, jsx) {
-  const html = renderJSXToHTML(jsx);
-  console.log(html);
-  res.setHeader('Content-Type', 'text/html');
-  res.end(html);
+    } else throw new Error('Cannot render an object.');
+  } else throw new Error('Not implemented.');
 }
